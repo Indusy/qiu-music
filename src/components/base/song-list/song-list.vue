c587db1e2edd7ecf0e545a7c14233ad47a891ef3
indusy <teririkawaii@outlook.com>
2022-06-08 16:28:30 +0800

    完善详情页面

--- src/components/base/song-list/song-list.vue
+++ src/components/base/song-list/song-list.vue
@@ -48,3 +48,65 @@ function getRankText(index) {
     </li>
   </ul>
 </template>
+
+<style lang="scss" scoped>
+.song-list {
+  margin: 0;
+  padding: 0;
+
+  .item {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    height: 64px;
+    font-size: 14px;
+
+    .rank {
+      flex: 0 0 25px;
+      width: 25px;
+      margin-right: 20px;
+      text-align: center;
+
+      .icon {
+        display: inline-block;
+        width: 25px;
+        height: 24px;
+        background-size: 25px 24px;
+
+        &.icon0 {
+          background-image: url(./first.png);
+        }
+
+        &.icon1 {
+          background-image: url(./second.png);
+        }
+
+        &.icon2 {
+          background-image: url(./third.png);
+        }
+      }
+    }
+
+    .content {
+      flex: 1;
+      overflow: hidden;
+
+      .name {
+        margin: 0;
+        @include no-wrap();
+        font-size: 18px;
+        color: $color-dark-9;
+      }
+
+      .desc {
+        margin: 0;
+        @include no-wrap();
+        margin-top: 4px;
+        font-size: 12px;
+        color: $color-dark-8;
+      }
+    }
+  }
+
+}
+</style>
